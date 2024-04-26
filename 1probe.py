72e2a82d27f06a66d7f7fd3626c6c76016e51902
Zver <tatysh571@gmail.com>
2024-04-26 19:16:09 +0300

    новый

--- 1probe.py
+++ 1probe.py
@@ -1,57 +1,63 @@
-# import random
-# import time
-# from collections import defaultdict
-# from threading import Thread
-#
-#
-# SKILL = (0, 10, 20, 30, 40, 50, 60, 70, 80, 90)
-#
-# class Knight(Thread):
-#     def init(self, name, skills, day, *args, **kwargs):
-#         super(Knight, self).init (*args,**kwargs)
-#         self.name = name
-#         self.skills = skills
-#         self.day = day
-#         self.catch = defaultdict(int)
-#     def run (self):
-#         self.catch = defaultdict(int)
-#         for skills in range(self.skills):
-#             print(f'{self.name} сражается день (дня) {skills} врагов', flush= True)
-#             _ = 5**random.choice(SKILL)
-#             if skills is None:
-#                 print(f'{self.name}: никто сегодня не напал')
-#             else:
-#                 print(f'{self.name} у меня сегодня {skills} врагов', flush= True)
-#                 self.catch[skills] += 1
-#         print(f'Итого {self.name} убил:')
-#         for skills, count in self.catch.items():
-#             print(f'{skills} - {count}')
-#
-# Sir_Lanselot = Knight(name = 'Sir_Lanselot', SKILL=20, day = 5)
-# Sir_Galahat = Knight(name = 'Sir_Galahat', SKILL=50, day = 3)
-#
-# print('.'*20, 'Sir_Lanselot, на нас напали!')
-# print('.'*20, 'Sir_Galahat, на нас напали!')
-# Sir_Lanselot.start()
-#
-# Sir_Galahat.start()
-# print('.'*5, 'Ждем')
-# Sir_Lanselot.join()
-# Sir_Galahat.join()
-# print('.'*5, 'Битвы закончились!')
+import random
+import time
+from collections import defaultdict
+from threading import Thread
+
+
+SKILL = (0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110)
+
+class Knight(Thread):
+    def __init__(self, name, skills, day, *args, **kwargs):
+        super(Knight, self).__init__(*args, **kwargs)
+        self.name = name
+        self.skills = skills
+        self.day = day
+        self.kill = defaultdict(int)
+    def run (self):
+        self.kill = defaultdict(int)
+        for skill in range(self.skills):
+            print(f'{self.name} сражается день (дня) {skill} врагов', flush=True)
+            _ = 5 ** (random.randint(50, 70) * 10000)
+            if skill is None:
+                print(f'{self.name}: никто сегодня не напал')
+            else:
+                print(f'{self.name} у меня сегодня {skill} врагов', flush=True)
+                self.kill[skill] += 1
+        print(f'Итого {self.name} убил:')
+        for skills, count in self.kill.items():
+            print(f'    {skills} - {count}')
+
+knight1 = Knight(name='Sir_Lancelot', skills=10, day=10)
+knight2 = Knight(name='Sir_Galahad', skills=20, day=5)
+
+print('.' * 20, 'Sir_Lancelot, на нас напали!')
+print('.' * 20, 'Sir_Galahad, на нас напали!')
+
+knight1.start()
+knight2.start()
+
+print('.' * 5, 'Ждем')
+
+knight1.join()
+knight2.join()
+
+print('.' * 5, 'Битвы закончились!')
+
+for skills in ():
+    print(f'Итого  {knight1.name} убил:')
+    for kill, count in knight1.skills.items():
+        print(f'    {Knight} - {count}')
+for skills in ():
+    print(f'Итого  {knight2.name} убил:')
+    for kill, count in knight2.skills.items():
+        print(f'    {Knight} - {count}')
 
 
 
-def all_variants(text):
-    for i in text:
-        yield i
-    for el in range(len(text)):
-        if el > 1:
-            a = 0
-        else:
-            a = el
-        yield text[a: el + 2]
 
 
-for substr in all_variants('abcd'):
-    print(substr)
+
+
+
+
+
