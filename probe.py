04dabcaf4c45e9943dcd5c54bba3ad5384f778c5
Tatysha45 <163041717+Tatysha45@users.noreply.github.com>
2024-04-26 19:29:57 +0300

    Delete probe.py

--- probe.py
+++
@@ -1,7 +0,0 @@
-# my_nambers = [1, 2, 5, 7, 12, 11, 35, 4, 89, 10]
-# result = [x * x for x in my_nambers if x % 2]
-# print(result)
-#
-# my_nambers = [1, 2, 5, 7, 12, 11, 35, 4, 89, 10]
-# result = map(lambda  x: x * x, filter(lambda x: x % 2, my_nambers))
-# print(list(result))
