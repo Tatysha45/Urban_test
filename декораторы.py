2149c642d7b73dd703ef2f1f07e02e2367df257a
Zver <tatysh571@gmail.com>
2024-05-06 22:12:01 +0300

    доработка

--- декораторы.py
+++ декораторы.py
@@ -1,19 +1,25 @@
-from math import isqrt
-
 def is_prime_decorator(sum_three):
     def wrapper(*args, **kwargs):
         result = sum_three(*args, **kwargs)
-        if result <= 1:
-            print(f"Результат {result} функции {sum_three.__name__} простое число")
-            return result
-        for i in range(2, isqrt(result) + 1):
-            if result % i == 0:
-                print(f"Результат {result} функции {sum_three.__name__} составное число")
-                return result
+        if is_prime_number(result):
+            print("Простое")
+        else:
+            print("Составное")
 
         return result
     return wrapper
+
+
 print()
+
+def is_prime_number(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 
 @is_prime_decorator
 def sum_three(a, b, c):
@@ -22,17 +28,3 @@ def sum_three(a, b, c):
 
 result = sum_three(3, 7, 5)
 print(result)
-
-
-
-
-
-
-
-
-
-# def sum_three(a, b, c):
-#     return a + b + c
-# numbers = [3, 7, 5]
-# sum = sum(numbers)
-# n = numbers
