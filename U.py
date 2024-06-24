502a9c1c989e44b329d68b234b833c3f5e9c5fc4
Zver <tatysh571@gmail.com>
2024-06-25 00:27:57 +0300

    new

--- U.py
+++ U.py
@@ -30,6 +30,17 @@ def add_gradient_background(image1, start_color, end_color):
 
 add_gradient_background(image, (255, 0, 0), (0, 0, 255))
 
+cv2.circle(image, (250, 250), 230, (255, 255, 255), thickness=5)
+
+cv2.line(image, (100, 150), (200, 150), (0, 0, 255), thickness=5)
+cv2.line(image, (300, 150), (400, 150), (0, 0, 255), thickness=5)
+cv2.line(image, (100, 150), (100, 320), (255, 0, 0), thickness=5)
+cv2.line(image, (200, 150), (200, 320), (255, 0, 0), thickness=5)
+cv2.line(image, (300, 150), (300, 320), (255, 0, 0), thickness=5)
+cv2.line(image, (400, 150), (400, 320), (255, 0, 0), thickness=5)
+cv2.ellipse(image, (250, 320), (150, 150), 0, 180, 0, (0, 255, 0), thickness=5)
+cv2.ellipse(image, (250, 320), (50, 50), 0, 180, 0, (0, 255, 0), thickness=5)
+
 cv2.imshow('Logo with Gradient Background', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
